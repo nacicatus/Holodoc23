390e61aa8f27f1ac5cef6e9c3b7b88a082d81896
SaurabhSikka <drsaurabhsikka@gmail.com>
2016-10-23 10:59:34 +0200

    Klingon

--- Holodoc23/ViewController.swift
+++ Holodoc23/ViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
+import TextToSpeechV1
 
 class ViewController: UIViewController {
+     let tts = TextToSpeech(username: "1ef14235-4608-4035-a929-cd95905d74ec", password: "1KiHAMEFriYj")
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func speakKlingon(sender: UIButton) {
+        tts.synthesize("Today is a good day to die",
+                       //voice: SynthesisVoice.GB_Kate,
+                       audioFormat: AudioFormat.WAV,
+                       failure: { error in
+                        print("error was generated \(error)")
+        }) { data in
+            
+            do {
+                self.player = try AVAudioPlayer(data: data)
+                self.player!.play()
+            } catch {
+                print("Couldn't create player.")
+            }
+            
+        }
     }
 
 
